3b83a33786622809f7d456544709699e1465f739
FerdinandChagas <ferdinandsc@yahoo.com.br>
2022-05-12 19:46:52 -0300

    Testando 1,2,3 ..

---
+++ Hello.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Hello {
+   
+
+    public static void main(String args[]){
+        ArrayList<String> teste = new ArrayList<String>();
+        teste.add("Teste");
+        System.out.println("Hello World! "+teste.get(0));
+    }
+}
